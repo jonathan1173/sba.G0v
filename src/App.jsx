12fac638e09710b0ed1c8cd4335c0030f6d7c3e5
jonathan1173 <159040650+jonathan1173@users.noreply.github.com>
2025-11-15 20:53:34 -0600

    Actualizar App.jsx
    blanco

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./shares/componentes/header";
 import Navbar from "./shares/componentes/navbar";
 import Home from "./feature/home/page/home";
 import AboutSba from "./feature/aboutSba/pages/about";
 import Footer from "./feature/home/components/PreFooter";
 import FooterBar from "./feature/home/components/footer";
-function App() {
 
+function App() {
   return (
     <Router>
-      //<Navbar />
+      {/* <Navbar /> */}
       <Routes>
         <Route path="No" element={<Home />} />
         <Route path="Noo" element={<AboutSba />} />
       </Routes>
-        //<Footer/>
-      //<FooterBar/>
+      {/* <Footer/> */}
+      {/* <FooterBar/> */}
     </Router>
   );
 }
