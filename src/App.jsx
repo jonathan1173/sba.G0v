0e70c0b0d0811361a1a7cdcee65608b28252cbe1
Jonathan Samuel <jonathan106e@gmail.com>
2026-02-03 20:55:07 -0600

    vulneracion y trafico

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import AboutSba from "./feature/aboutSba/pages/about";
 import Footer from "./feature/home/components/PreFooter";
 import FooterBar from "./feature/home/components/footer";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// 1. Asegúrate de importar desde "/react"
+import { Analytics } from "@vercel/analytics/react"; 
 
 function App() {
   return (
@@ -11,11 +13,13 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-
         <Route path="/organization/sba-leadership" element={<AboutSba />} />
       </Routes>
       <Footer/>
       <FooterBar/>
+      
+      {/* 2. Colócalo aquí, al final de todo pero dentro del Router */}
+      <Analytics />
     </Router>
   );
 }
